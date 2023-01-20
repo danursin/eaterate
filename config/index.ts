3923d910a97dcceb083bc9c5ce729480b076771e
danursin <danursin@gmail.com>
2023-01-19 22:18:28 -0600

    fix: avoid reserved AWS environment variable names

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION;
-const tableName = process.env.AWS_DYNAMODB_TABLENAME;
+const accessKeyId = process.env.X_AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.X_AWS_SECRET_ACCESS_KEY;
+const region = process.env.X_AWS_REGION;
+const tableName = process.env.X_AWS_DYNAMODB_TABLENAME;
 
 export default {
     secretAccessKey,
